87bf1d7c973ee012109da2dec5adbee784ad1d7d
leasorssini <dlegasr@gmail.com>
2025-01-29 10:54:13 -0300

    teste

--- infra/output.tf
+++ infra/output.tf
@@ -3,10 +3,10 @@
 # Nome e ARN da tabela video_metadata
 output "video_metadata_table_name" {
   description = "Nome da tabela video_metadata"
-  value       = aws_dynamodb_table.video_metadata.name
+  value       = aws_dynamodb_table.metadata_table.name
 }
 
 output "video_metadata_table_arn" {
   description = "ARN da tabela video_metadata"
-  value       = aws_dynamodb_table.video_metadata.arn
+  value       = aws_dynamodb_table.metadata_table.arn
 }
