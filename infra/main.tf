0370c5d714be6834a6058f97eb93f9af7f4b9c8c
leasorssini <dlegasr@gmail.com>
2025-01-29 10:45:15 -0300

    teste

--- infra/main.tf
+++ infra/main.tf
@@ -1,47 +1,29 @@
+######### PROVEDOR AWS #################################################
 provider "aws" {
   region = var.aws_region
 }
 
-# Tabela video_metadata
-resource "aws_dynamodb_table" "video_metadata" {
-  name           = var.metadata_table_name
+######### TABELA video_metadata #########################################
+resource "aws_dynamodb_table" "metadata_table" {
+  name           = "${var.prefix_name}-${var.metadata_table_name}-table"
   billing_mode   = "PROVISIONED" # Free Tier
   read_capacity  = 1
   write_capacity = 1
-  hash_key       = "videoId"
-  range_key      = "username"
+  hash_key       = "video_id"
+  range_key      = "user_name"
 
   attribute {
-    name = "videoId"
+    name = "video_id"
     type = "S" # String
   }
 
   attribute {
-    name = "username"
+    name = "user_name"
     type = "S" # String
   }
 
   tags = {
     Name        = var.metadata_table_name
-    Environment = var.environment
-  }
-}
-
-# Tabela video_status
-resource "aws_dynamodb_table" "video_status" {
-  name           = var.status_table_name
-  billing_mode   = "PROVISIONED" # Free Tier
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "videoId"
-
-  attribute {
-    name = "videoId"
-    type = "S" # String
-  }
-
-  tags = {
-    Name        = var.status_table_name
     Environment = var.environment
   }
 }
